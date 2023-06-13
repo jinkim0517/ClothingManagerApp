230b281c0e33e4e79284d5e3afbdbdc08e3ab7f3
jinkim0517 <jkim71@students.cs.ubc.ca>
2023-06-13 15:52:47 -0700

    Replaced Delete button

--- app/src/main/java/com/example/clothinginventoryapp/ui/MainScreen.kt
+++ app/src/main/java/com/example/clothinginventoryapp/ui/MainScreen.kt
@@ -8,12 +8,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -81,7 +79,7 @@ fun MainScreen(navController: NavController,
                     .width(300.dp),
             ) {
                 Text(
-                    text = "Delete Clothing",
+                    text = "Browse Suggestions",
                     fontSize = 30.sp,
                     modifier = Modifier.padding(5.dp)
                 )
